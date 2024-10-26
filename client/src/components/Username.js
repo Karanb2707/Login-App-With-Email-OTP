ef832451a06bc2791f4fd3e7977011eb77692e2e
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2024-10-26 22:28:21 +0530

    Username UI Done

---
+++ client/src/components/Username.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import avatar from '../assets/profile.png';
+import styles from '../styles/Username.module.css';
+
+export default function Username() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Hello Again!</h4>
+            <span className="py-4 text-small text-center text-white">
+              Explore More with Us
+            </span>
+          </div>
+          
+          <form className="py-1">
+            <div className="profile flex justify-center py-4">
+              <img className={styles.profile_img} src={avatar} alt="avatar" />
+            </div>
+
+            <div className="textbox flex flex-col items-center gap-6">
+              <input className={styles.textbox} type="text" placeholder="Username" />
+              <button className={styles.btn} type="submit">Let's Go</button>
+            </div>
+            
+            <div className="text-center py-4">
+              <span className="text-white">
+                Not a Member? <Link className="text-red-950" to="/register">Register Now</Link>
+              </span>
+            </div>
+          </form>
+          
+        </div>
+      </div>
+    </div>
+  );
+}

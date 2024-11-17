bd4cdd78e3d3fdc8573bd8a136b8e2080708e795
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2024-11-17 18:42:23 +0530

    DB Connection set continue with routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from 'cors';
 import morgan from "morgan";
+import connect from "./database/conn.js";
+import { errors } from "mongodb-memory-server";
 
 const app = express();
 
@@ -9,17 +11,25 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.disable('x-powerd-by');
 
-
-
 const port = 8000;
 
 app.get('/',(req,res) => {
     res.status(201).json("Home GET Request");
 })
 
-app.listen(port,() => {
-    console.log(`Server connected to http:://localhost:${port}`);
+connect().then(() => {
+    try {
+        app.listen(port,() => {
+            console.log(`Server connected to http:://localhost:${port}`);
+        })
+    } catch (error) {
+        console.log('Cannot connnect to the server');
+    }
+}).catch(error => {
+    console.log("Invalid Database Connection!!");
 })
+
+
 
 
 
